87458ab878f97e98de6e9e75752d4678d0218c1e
Mohd Fakhri Aluwe <42363806+fakhrialuwe@users.noreply.github.com>
2019-07-31 10:34:27 +0800

    Delete BranchControllerSpec.test.ts

--- src/admin/branch/__tests__/BranchControllerSpec.test.ts
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const BranchControllerSpec = require('../branch.controller.spec.ts');
-
-describe('BranchControllerSpec', () => {
-  it('has a test', () => {
-    assert(false, 'BranchControllerSpec should have a test');
-  });
-});

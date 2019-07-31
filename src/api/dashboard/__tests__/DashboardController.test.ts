8a060a46029a33e28cfc0e031083ad1092c274a6
Mohd Fakhri Aluwe <42363806+fakhrialuwe@users.noreply.github.com>
2019-07-31 10:33:42 +0800

    Delete DashboardController.test.ts

--- src/api/dashboard/__tests__/DashboardController.test.ts
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const DashboardController = require('../dashboard.controller.ts');
-
-describe('DashboardController', () => {
-  it('has a test', () => {
-    assert(false, 'DashboardController should have a test');
-  });
-});

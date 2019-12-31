c90d428850238e8a62f40952e17800c700366794
Fakhri <fakhrialuwe@rocketmail.com>
2019-12-31 17:14:11 +0800

    get pending and approve list from 1 week before

--- src/admin/approval-override/pending-leave.service.ts
+++ src/admin/approval-override/pending-leave.service.ts
@@ -3,34 +3,71 @@ import { UserprofileDbService } from 'src/api/userprofile/db/userprofile.db.serv
 import { CompanyDbService } from '../company/company.service';
 import { LeavetypeService } from '../leavetype/leavetype.service';
 
+/**
+ * Get all pending leave service function
+ *
+ * @export
+ * @class PendingLeaveService
+ */
 @Injectable()
 export class PendingLeaveService {
+  /**
+   *Creates an instance of PendingLeaveService.
+   * @param {UserprofileDbService} userprofileDbService user profile db service
+   * @param {CompanyDbService} companyDbService company db service
+   * @param {LeavetypeService} leavetypeService leave type db service
+   * @memberof PendingLeaveService
+   */
   constructor(
     private readonly userprofileDbService: UserprofileDbService,
     private readonly companyDbService: CompanyDbService,
     private readonly leavetypeService: LeavetypeService
   ) { }
 
+  /**
+   * Get company list using callback function
+   *
+   * @param {string} TENANT_GUID
+   * @returns
+   * @memberof PendingLeaveService
+   */
   public async getCompanyList(TENANT_GUID: string) {
-    const cbCompany = () => {
-      return new Promise((resolve, reject) => {
-        this.companyDbService.findByFilterV2([], ['(TENANT_GUID=' + TENANT_GUID + ')']).subscribe(
-          data => {
-            resolve(data);
-          }, err => {
-            return reject(err);
-          }
-        )
-      })
-    }
-    let companyList = await cbCompany() as any[];
-    return companyList;
+    return await this.runService(this.companyDbService.findByFilterV2([], ['(TENANT_GUID=' + TENANT_GUID + ')']));
   }
 
+  /**
+   * Get leavetype list using callback function
+   *
+   * @param {string} TENANT_GUID
+   * @returns
+   * @memberof PendingLeaveService
+   */
   public async getLeavetypeList(TENANT_GUID: string) {
-    const cbLeaveType = () => {
+    return await this.runService(this.leavetypeService.findByFilterV2([], ['(TENANT_GUID=' + TENANT_GUID + ')']));
+  }
+
+  /**
+   * Get personal info using callback function
+   *
+   * @param {string[]} userGuid
+   * @returns
+   * @memberof PendingLeaveService
+   */
+  public async getUserInfo(userGuid: string[]) {
+    return await this.runService(this.userprofileDbService.findByFilterV2([], ['(USER_GUID IN (' + userGuid + ')']));
+  }
+
+  /**
+   * Run callback function
+   *
+   * @param {*} method
+   * @returns
+   * @memberof PendingLeaveService
+   */
+  public async runService(method) {
+    const cbService = () => {
       return new Promise((resolve, reject) => {
-        this.leavetypeService.findByFilterV2([], ['(TENANT_GUID=' + TENANT_GUID + ')']).subscribe(
+        method.subscribe(
           data => {
             resolve(data);
           }, err => {
@@ -39,24 +76,7 @@ export class PendingLeaveService {
         )
       })
     }
-    let leaveTypeList = await cbLeaveType() as any[];
-    return leaveTypeList;
-  }
-
-  public async getUserInfo(userGuid: string[]) {
-    const cbInfo = () => {
-      return new Promise((resolve, reject) => {
-        this.userprofileDbService.findByFilterV2([], ['(USER_GUID IN (' + userGuid + ')']).subscribe(
-          data => {
-            resolve(data);
-          }, err => {
-            return reject(err);
-          }
-        )
-      });
-    }
-    let resultAll = await cbInfo() as any[];
-    return resultAll;
+    return await cbService();
   }
 
 }

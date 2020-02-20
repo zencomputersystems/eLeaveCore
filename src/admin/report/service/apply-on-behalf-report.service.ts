e8bcbe915094503f3bdb7aa43f010cc5618f09f2
Fakhri <fakhrialuwe@rocketmail.com>
2020-02-20 16:00:02 +0800

    edit apply on behalf report from id to value

--- src/admin/report/service/apply-on-behalf-report.service.ts
+++ src/admin/report/service/apply-on-behalf-report.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { ReportDBService } from './report-db.service';
-import { map } from 'rxjs/operators';
+import { map, mergeMap } from 'rxjs/operators';
 import { ApplyOnBehalfReportDto } from '../dto/apply-on-behalf-report.dto';
+import { PendingLeaveService } from 'src/admin/approval-override/pending-leave.service';
 
 @Injectable()
 export class ApplyOnBehalfReportService {
-  constructor(private readonly reportDBService: ReportDBService) { }
+  constructor(private readonly reportDBService: ReportDBService,
+    private readonly pendingLeaveService: PendingLeaveService) { }
   getApplyOnBehalfData([tenantId]: [string]) {
     return this.reportDBService.leaveTransactionDbService.findByFilterV2([], [`(APPLIED_ON_BEHALF=1)`, `(TENANT_GUID=${tenantId})`]).pipe(
-      map(res => {
+      mergeMap(async res => {
+        let resultAll = await this.pendingLeaveService.getAllUserInfo(res[0].TENANT_GUID) as any[];
+        return { res, resultAll };
+      }),
+      map(result => {
+        let { res, resultAll } = result;
         let userIdList = [];
         res.forEach(element => {
+          let resultUser = resultAll.find(x => x.USER_GUID === element.USER_GUID);
+
           let applyOnBehalfReportDto = new ApplyOnBehalfReportDto;
 
           applyOnBehalfReportDto.userGuid = element.USER_GUID;
-          applyOnBehalfReportDto.employeeNo = 'wf';
-          applyOnBehalfReportDto.employeeName = 'dw';
+          applyOnBehalfReportDto.employeeNo = resultUser.STAFF_ID;
+          applyOnBehalfReportDto.employeeName = resultUser.FULLNAME;
           applyOnBehalfReportDto.yearService = 2;
           applyOnBehalfReportDto.leaveType = element.LEAVE_TYPE_GUID;
           applyOnBehalfReportDto.applicationDate = element.CREATION_TS;

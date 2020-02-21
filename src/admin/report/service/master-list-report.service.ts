77747068dbf6f20e451a9707ab231f82a0ccd291
Fakhri <fakhrialuwe@rocketmail.com>
2020-02-21 11:26:44 +0800

    add designation to master list report

--- src/admin/report/service/master-list-report.service.ts
+++ src/admin/report/service/master-list-report.service.ts
@@ -14,6 +14,7 @@ export class MasterListReportService {
         let companyList = await this.pendingLeaveService.getCompanyList(res[0].TENANT_GUID) as any[];
         let resultAll = await this.pendingLeaveService.getAllUserInfo(res[0].TENANT_GUID) as any[];
         return await { res, companyList, resultAll };
+
       }),
       mergeMap(async result => {
         let userIdList = []
@@ -21,37 +22,29 @@ export class MasterListReportService {
         res.forEach(element => {
           let masterListReportDTO = new MasterListReportDto;
           let managerUser = element.MANAGER_USER_GUID != null ? resultAll.find(x => x.USER_GUID === element.MANAGER_USER_GUID) : [];
-          // console.log(managerUser);
-          // console.log(element.FULLNAME);
-          // console.log(managerUser.FULLNAME);
+
           let managerName = managerUser != undefined ? managerUser.hasOwnProperty('FULLNAME') ? managerUser.FULLNAME : '' : '';
 
           let resultCompany = element.TENANT_COMPANY_GUID != null ? companyList.find(x => x.TENANT_COMPANY_GUID === element.TENANT_COMPANY_GUID) : [];
-          // console.log(element.MANAGER_USER_GUID);
-          // console.log(element.USER_GUID);
-
-
-          // console.log(managerName);
 
           masterListReportDTO.userGuid = element.USER_GUID;
           masterListReportDTO.employeeNo = element.STAFF_ID;
           masterListReportDTO.employeeName = element.FULLNAME;
           masterListReportDTO.companyName = resultCompany.NAME || null;
           masterListReportDTO.department = element.DEPARTMENT;
+          masterListReportDTO.designation = element.DESIGNATION;
           masterListReportDTO.email = element.EMAIL;
           masterListReportDTO.approver = managerName;
           masterListReportDTO.joinDate = element.JOIN_DATE;
           masterListReportDTO.confirmDate = element.CONFIRMATION_DATE;
           masterListReportDTO.resignDate = element.RESIGNATION_DATE;
-          masterListReportDTO.status = element.STATUS_ACTIVATION;
+          masterListReportDTO.status = element.EMPLOYEE_STATUS;
 
-          // console.log(masterListReportDTO);
           userIdList.push(masterListReportDTO);
-
         });
         return await userIdList;
+
       }), map(res => {
-        console.log(res);
         return res;
       })
 

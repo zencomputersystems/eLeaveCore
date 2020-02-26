e8afa85708601c9918e0e1edfcf033fc450e385f
Fakhri <fakhrialuwe@rocketmail.com>
2020-02-26 16:58:42 +0800

    update leave adjustment report api

--- src/admin/leave-adjustment/model/leave-adjustment-log.model.ts
+++ src/admin/leave-adjustment/model/leave-adjustment-log.model.ts
@@ -1,6 +1,7 @@
 import { CreateUpdateModel } from '../../../common/model/create-update.model';
 export class LeaveAdjustmentLogModel extends CreateUpdateModel {
   LEAVE_ADJUSTMENT_LOG_GUID: string;
+  TENANT_GUID: string;
   USER_GUID: string;
   LEAVE_TYPE_GUID: string;
   ADJUSTMENT: string;

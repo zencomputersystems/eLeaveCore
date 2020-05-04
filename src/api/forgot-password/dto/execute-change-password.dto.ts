6116dae2db0337050c1561772d2fa1cfb4610178
Fakhri <fakhrialuwe@rocketmail.com>
2020-05-05 00:07:43 +0800

    update change password store encrypted

--- src/api/forgot-password/dto/execute-change-password.dto.ts
+++ src/api/forgot-password/dto/execute-change-password.dto.ts
@@ -2,7 +2,7 @@ import { ChangePasswordDto } from './change-password.dto';
 import { ApiModelProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 export class ExecuteChangePasswordDto extends ChangePasswordDto {
-  @ApiModelProperty({ description: 'encrypted password', example: 'dnwkejfnoiwenf' })
+  @ApiModelProperty({ description: 'encrypted password', example: 'YWJjMTIz' })
   @IsString()
   @IsNotEmpty()
   oldPassword: string;

96c8f2a34abf9ab4f57cea6453bc78f250d9f6ce
Fakhri <fakhrialuwe@rocketmail.com>
2020-05-19 17:15:49 +0800

    add https scheme

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('leave')
     .addBearerAuth('Authorization', 'header', 'apiKey')
+    .setSchemes("http", "https")
     .build();
 
   const document = SwaggerModule.createDocument(app, options);

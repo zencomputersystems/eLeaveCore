b6cd7fa1efd366a5af877a41da8663aad5b4c745
Fakhri <fakhrialuwe@rocketmail.com>
2020-11-12 15:02:55 +0800

    Update dashboard medical-leave api

--- src/common/helper/basic-functions.ts
+++ src/common/helper/basic-functions.ts
@@ -5,6 +5,7 @@ import moment = require('moment');
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Response } from 'express';
+import { LeaveTypePropertiesXmlDTO } from 'src/admin/leavetype-entitlement/dto/xml/leavetype-properties.xml.dto';
 
 //  ---------------------------------------------------------------------------------------------------------------------------
 /**
@@ -171,4 +172,80 @@ export async function runServiceCallback(method) {
     })
   }
   return await cbService();
+}
+
+export function dateToValidate([dateJoin, dateConfirm, policyEntitlement]: [any, any, LeaveTypePropertiesXmlDTO]) {
+  if (policyEntitlement.leaveEntitlementType.toUpperCase() == "PRORATED FROM DATE-OF-CONFIRM TO CURRENT MONTH" || policyEntitlement.leaveEntitlementType.toUpperCase() == "PRORATED FROM DATE-OF-CONFIRM TO END OF YEAR") {
+    return dateConfirm;
+  }
+  else {
+    return dateJoin;
+  }
+}
+
+export function entitledCount([dateOfSet, policyJson]: [any, LeaveTypePropertiesXmlDTO]) {
+  let totalentitled = 0;
+  let entitledTillMonth = 0;
+  for (var i = 1; i <= 12; i++) {
+
+    var d = new Date(new Date().getFullYear(), i, 1, 1);
+
+    let yearOfService = moment.duration(moment(d).diff(dateOfSet)).asMonths();
+
+    yearOfService = yearOfService / 12;
+
+    policyJson.levels.leaveEntitlement = Array.isArray(policyJson.levels.leaveEntitlement) ? policyJson.levels.leaveEntitlement : [policyJson.levels.leaveEntitlement];
+    let currentLevel = policyJson.levels.leaveEntitlement.find(x => x.serviceYearFrom <= yearOfService && x.serviceYearTo > yearOfService);
+
+    if (currentLevel != undefined) {
+
+      let totalDaysInMonth = moment(d).subtract(1, 'days').format('DD');
+
+      let daysOfService = moment.duration(moment(d).diff(dateOfSet)).asDays();
+
+      let byMonthEntitled = currentLevel.entitledDays / 12;
+
+      if (daysOfService <= parseInt(totalDaysInMonth)) {
+        byMonthEntitled = byMonthEntitled / parseInt(totalDaysInMonth) * daysOfService;
+      }
+
+      totalentitled += byMonthEntitled;
+
+      if (new Date().getMonth() == d.getMonth()) {
+        entitledTillMonth = totalentitled;
+      }
+
+    }
+
+  }
+
+  let yearOfServiceFull = moment.duration(moment().diff(dateOfSet)).asYears();
+
+  let currentLevel = policyJson.levels.leaveEntitlement.find(x => x.serviceYearFrom <= yearOfServiceFull && x.serviceYearTo > yearOfServiceFull);
+
+  let monthOfService = moment.duration(moment().diff(dateOfSet)).asMonths();
+
+  let after12Month = 0;
+  if (monthOfService > 12) {
+    let yearService = (monthOfService - 12) / 12;
+
+    let levelAfterDeduct = policyJson.levels.leaveEntitlement.find(x => x.serviceYearFrom <= yearService && x.serviceYearTo > yearService);
+    after12Month = levelAfterDeduct.entitledDays;
+  }
+
+  let entitledDaysFinal = 0;
+
+  if (policyJson.leaveEntitlementType.toUpperCase() == "ENTITLED IN FULL") {
+    entitledDaysFinal = currentLevel.entitledDays;
+    totalentitled = currentLevel.entitledDays;
+  } else if (policyJson.leaveEntitlementType.toUpperCase() == "PRORATED FROM DATE-OF-JOIN TO CURRENT MONTH" || policyJson.leaveEntitlementType.toUpperCase() == "PRORATED FROM DATE-OF-CONFIRM TO CURRENT MONTH") {
+    entitledDaysFinal = entitledTillMonth;
+  } else if (policyJson.leaveEntitlementType.toUpperCase() == "PRORATED FROM DATE-OF-JOIN TO END OF YEAR" || policyJson.leaveEntitlementType.toUpperCase() == "PRORATED FROM DATE-OF-CONFIRM TO END OF YEAR") {
+    entitledDaysFinal = totalentitled;
+  } else if (policyJson.leaveEntitlementType.toUpperCase() == "LEAVE ENTITLED AFTER 12 MONTHS") {
+    entitledDaysFinal = after12Month;
+    totalentitled = entitledDaysFinal;
+  }
+
+  return { entitledDaysFinal, totalentitled };
 }
